a1e1347d0887d13b4627db088e40c6e4276fafd7
Eduard <emekler0729@gmail.com>
2021-09-14 15:43:45 -0500

    added styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './components/Calculator';
+import './scss/index.scss';
 
 if (module.hot) module.hot.accept();
 
